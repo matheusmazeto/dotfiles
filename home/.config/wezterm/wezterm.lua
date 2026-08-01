a07dc4886e92325019828d6e7f486e5cc831d4f2
Matheus Mazeto <mgmazeto@gmail.com>
2026-08-01 11:35:19 -0300

    add: set initial columns and rows for WezTerm configuration

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -18,6 +18,8 @@ end)
 config.color_scheme = "rose-pine-moon"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
+config.initial_cols = 140
+config.initial_rows = 40
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
